b32ae97851e5df3221a7e247dda3e8e98a07af78
stefan judis <stefanjudis@gmail.com>
2021-10-05 17:43:26 +0200

    Initial commit

---
+++ data/posts.js
@@ -0,0 +1,167 @@
+export default [
+  {
+    slug: "welcome",
+    title: "Welcome to the Contentful Jamstack conf workshop!",
+    coverImage: {
+      url: "/hero.png",
+    },
+    date: "2021-10-05T00:00:00.000+02:00",
+    author: {
+      name: "Stefan",
+      picture: {
+        url: "/stefan.jpg/",
+      },
+    },
+    excerpt:
+      "We're excited to have you around! And today we'll build our own Next.js/Contentful blog from scratch!!!",
+    content: {
+      json: {
+        nodeType: "document",
+        data: {},
+        content: [
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Hello world people!!!",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Lorem Ipsum",
+                marks: [{ type: "bold" }],
+                data: {},
+              },
+              {
+                nodeType: "text",
+                value:
+                  " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+        ],
+      },
+    },
+  },
+  {
+    slug: "another-post",
+    title: "Another example post!",
+    coverImage: {
+      url: "/hero.png",
+    },
+    date: "2021-10-05T00:00:00.000+02:00",
+    author: {
+      name: "Stefan",
+      picture: {
+        url: "/stefan.jpg/",
+      },
+    },
+    excerpt:
+      "We're excited to have you around! And today we'll build our own Next.js/Contentful blog from scratch!!!",
+    content: {
+      json: {
+        nodeType: "document",
+        data: {},
+        content: [
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Hello world people!!!",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Lorem Ipsum",
+                marks: [{ type: "bold" }],
+                data: {},
+              },
+              {
+                nodeType: "text",
+                value:
+                  " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+        ],
+      },
+    },
+  },
+  {
+    slug: "second-post",
+    title: "And the last example post",
+    coverImage: {
+      url: "/hero.png",
+    },
+    date: "2021-10-05T00:00:00.000+02:00",
+    author: {
+      name: "Stefan",
+      picture: {
+        url: "/stefan.jpg/",
+      },
+    },
+    excerpt:
+      "We're excited to have you around! And today we'll build our own Next.js/Contentful blog from scratch!!!",
+    content: {
+      json: {
+        nodeType: "document",
+        data: {},
+        content: [
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Hello world people!!!",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+          {
+            nodeType: "paragraph",
+            content: [
+              {
+                nodeType: "text",
+                value: "Lorem Ipsum",
+                marks: [{ type: "bold" }],
+                data: {},
+              },
+              {
+                nodeType: "text",
+                value:
+                  " is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+                marks: [],
+                data: {},
+              },
+            ],
+            data: {},
+          },
+        ],
+      },
+    },
+  },
+];
---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import Head from "next/head";
+
+export default function Index({ preview, allPosts }) {
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
+  return (
+    <>
+      <Layout preview={preview}>
+        <Head>
+          <title>Next.js Blog Example with Contentful</title>
+        </Head>
+        <Container>
+          {heroPost && (
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              date={heroPost.date}
+              author={heroPost.author}
+              slug={heroPost.slug}
+              excerpt={heroPost.excerpt}
+            />
+          )}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export async function getStaticProps({ preview = false }) {
+  const allPosts = (await getAllPostsForHome()) ?? [];
+
+  return {
+    props: { allPosts },
+  };
+}
